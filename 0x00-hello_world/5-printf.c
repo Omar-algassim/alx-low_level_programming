b1417bf0ebb7431c7744c2e6f97503babe58bcda
Omar-algassim <omarggg2@gmail.com>
2023-08-10 17:27:19 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,13 @@
 
 /**
  *
- * main - entry point 
+ * main - entry point
  *
  * always return 0
- * */ 
+ */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
+	printf("with proper grammar, but the outcome is a piece of art,\n");	
 	return (0);
 }
