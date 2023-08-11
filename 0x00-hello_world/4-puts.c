cd445115692cd0b4904a67c94993d0e60d3bf8e2
Omar-algassim <omarggg2@gmail.com>
2023-08-10 17:11:41 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #!/bin/bash
-main<stdio.h>
+
+#include <stdio.h>
 
 main int (void)
 		{
-		puts("\""Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
 			return(0);
 		}
