05c9f9b5896c745c1fdaab618ded7b6192e0bb96
Omar-algassim <omarggg2@gmail.com>
2023-08-12 08:03:13 -0700

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != e && low!=q)
+			putchar(low);
+}
+	putchr('\n');
+	return (0);
+	}
