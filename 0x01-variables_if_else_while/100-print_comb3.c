d074195f62f68beddf6dd4e228402e3529d854fa
Omar-algassim <omarggg2@gmail.com>
2023-08-12 11:33:27 -0700

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,10 +7,10 @@
 int main(void)
 {
 	int x, y;
-	
-	for (x = 0; x < 9; x++)
+
+	for (x = '0'; x < '9'; x++)
 	{
-		for (y = 1; y <= 9 ; y++)
+		for (y = '1'; y <= '9'; y++)
 		{
 			putchar(x);
 			putchar(y);
