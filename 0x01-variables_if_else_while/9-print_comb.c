03ba431a99b0a25cee0e9570be78cba3919361ca
Omar-algassim <omarggg2@gmail.com>
2023-08-12 09:06:43 -0700

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,18 +2,20 @@
 /**
  * main - Entry point
  *
- * REturn: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int d;
 
 	for (d = '0'; d <= '9'; d++)
 	{
 		putchar(d);
+	for (d = '0'; d <= '8'; d++)
+	{	
 		putchar(',');
 		putchar(' ');
-	}
+	}}
 	putchar('\n');
 	return (0);
 }
