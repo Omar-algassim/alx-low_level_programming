9e0f2bae35631d91e43cb4bf9aa9952108bcd9d8
Omar-algassim <omarggg2@gmail.com>
2023-08-12 07:44:43 -0700

    update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@ int main(void)
 	char low;
 	char upp;
 
-	for(low = 'a'; low <= 'z'; low++)
-            putchar(low);
-	for(upp = 'A'; upp <= 'Z'; upp++)
-    	    putchar(upp);
-    putchar('\n');
-	    return (0);
+	for (low = 'a'; low <= 'z'; low++)
+	   	putchar(low);
+	for (upp = 'A'; upp <= 'Z'; upp++)
+		putchar(upp);
+     	putchar('\n');
+      	return (0);
 }
 
