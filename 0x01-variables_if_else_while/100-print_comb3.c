ad190f2f6517d6555ce6d2369f4099f4c5e8873d
Omar-algassim <omarggg2@gmail.com>
2023-08-12 11:28:08 -0700

    update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alawys 0 (Success)
+ */
+int main(void)
+{
+	int x, y, z;
+	
+	for (x = 0; x < 9; x++)
+	{
+		for (y = 1; y <= 9 ; y++)
+		{
+			putchar(x);
+			putchar(y);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
