ec9e0a31cf2acee75518c9c22fa9dcc1e7fc1baa
Omar-algassim <omarggg2@gmail.com>
2023-08-12 06:47:56 -0700

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low;
+	while(low = 'a'; low <= 'z'; low++)
+		putchr(low);
+	putchar('\n');
+	
+	return(0);
+}
