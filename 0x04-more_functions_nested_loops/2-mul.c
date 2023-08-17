4e8154146f404f988bd6ba6ed70b68fea7892aa0
Omar-algassim <omarggg2@gmail.com>
2023-08-17 12:00:20 -0700

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul is function multiply tow numbe
+ * @a is first numbeer
+ * @b is second number
+ * Return: the result
+ */
+int mul(int a, int b);
+{
+return (a * b);
+}
