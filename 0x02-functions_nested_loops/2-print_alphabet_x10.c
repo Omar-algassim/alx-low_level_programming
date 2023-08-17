72eeecd355445f8bfcbe4bbe61ebd7f44ae6fcd4
Omar-algassim <omarggg2@gmail.com>
2023-08-16 17:58:32 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print 10x alphabet
+ * written by omar
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+{
+char l;
+int x;
+
+for (x = 0; x < 10; x++)
+{
+	for (l = 'a'; l < 'z'; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n')
+}
+}
