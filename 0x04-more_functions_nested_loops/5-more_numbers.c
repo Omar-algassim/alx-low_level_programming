18411d461d16d105cca11e66e2811bea9c5cf807
Omar-algassim <omarggg2@gmail.com>
2023-08-17 15:39:03 -0700

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - print number from 0 to 14
+ *
+ * Return: always 0
+ */
+
+void more_numbers(void)
+{
+	int n, i;
+
+
+	for (i = 0; i <= 10; i++)
+{
+	_putchar('\n');
+	for (n = 0; n <= 14; n++)
+	{
+		_putchar(n);
+
+	}
+}
+_putchar('\n')
+}
