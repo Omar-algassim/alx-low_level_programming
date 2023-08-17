e9fe878f96ba6b37e95574beed52bfc4e01c5bf7
Omar-algassim <omarggg2@gmail.com>
2023-08-17 09:39:22 -0700

    update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include 'main.h'
+
+/**
+*-isupper checks for uppercase character
+*written by omar
+* Returns 1 if c is uppercase
+*Returns 0 otherwise
+*/
+
+int _isupper(int c);
+{
+char u;
+
+if (u <= 'a' && u >= 'z')
+{
+	return (1);
+
+		else 
+			return (0);
+}
+}
