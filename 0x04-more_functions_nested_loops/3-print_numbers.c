df740cf3f172bd884efbafa0f69ad2a33402aeaf
Omar-algassim <omarggg2@gmail.com>
2023-08-17 12:29:23 -0700

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,19 @@
 
 /**
  * print_numbers - print number from 0 to 9
- *
+ *_putchar - writes the character c to stdout
+ * @c: The character to print
  * Return: always 0
  */
 void print_numbers(void)
+int _putchar(char c)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
+	{	
 		_putchar(i);
+	}
+	_putchar('\n');
 	return (0);
 }
