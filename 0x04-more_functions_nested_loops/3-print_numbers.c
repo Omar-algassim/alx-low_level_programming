7dc7e130f6dd27eb08e383a6d54cf1ef24b963d9
Omar-algassim <omarggg2@gmail.com>
2023-08-17 15:04:16 -0700

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,10 +2,11 @@
 /**
 *print_numbers - function that print all numbers from
 *0 to 9
-*
+*_putchar - writes the character c to stdout
 *Return: returns nothing
 */
-void print_numbers(void)
+void print_numbers(void);
+int _putchar(char c);
 {
 int number = 48;
 for (number = 48; number < 58; number++)
