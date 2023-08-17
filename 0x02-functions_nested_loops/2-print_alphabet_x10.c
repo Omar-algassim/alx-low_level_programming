652bb2664468471daa9c4eab8f9214a396584062
Omar-algassim <omarggg2@gmail.com>
2023-08-16 18:00:43 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
  * written by omar
  * Return: 0
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 char l;
 int x;
 
 for (x = 0; x < 10; x++)
 {
-	for (l = 'a'; l < 'z'; l++)
+	for (l = 'a'; l <= 'z'; l++)
 	{
 		_putchar(l);
 	}
