32728be08da1ba050a35a7b701138eca591f5160
Omar-algassim <omarggg2@gmail.com>
2023-08-17 12:04:46 -0700

    update

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 /**
  * mul - is function multiply tow numbe
- * @a is first numbeer
- * @b is second number
+ * *@a: is first numbeer
+ * *@b: is second number
  * Return: the result
  */
 int mul(int a, int b)
