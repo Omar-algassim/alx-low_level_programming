a82a0d5bc332145d90d156e365a40953e1b31751
Omar-algassim <omarggg2@gmail.com>
2023-08-17 16:40:23 -0700

    update

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - print new line if it is not zero 
+ *
+ * Return: Always 0
+ */
+
+void print_line(int n);
+
+while (n-- > 0)
+	{
+_putchar('_');
+}
+_putchar('\n');
+}
