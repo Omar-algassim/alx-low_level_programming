1463fb44aef746f3756264c3787f09ec4f838c58
Omar-algassim <omarggg2@gmail.com>
2023-08-17 17:40:07 -0700

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,12 +15,18 @@ while (n++ < 100)
 	if (((n % 5) == 0) && ((n % 3) == 0))
 		printf("FizzBuzz ");
 	else if ((n % 3) == 0)
-		printf("Fizz ");
-	else if ((n % 5) == 0)
-		printf("Buzz ");
-	else
-	printf("%d ", n);
+		else if ((num % 5) == 0
+{
+if (num != 100)
+printf("Buzz ");
+
+else
+printf("Buzz");
+}
+
+else
+printf("%d ", num);
+
 printf("\n");
 return (0);
-
 }
