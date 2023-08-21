bd0054eac1aaaa8fce3d937495148cb72671474a
Omar-algassim <omarggg2@gmail.com>
2023-08-21 15:07:50 -0700

    update array

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -rest the pointer to 98
+ * @n : pointer to varisable to update
+ *written by omar
+ * Return: nothing
+ */
+
+void reset_to_98(int *n);
+{
+	printf("the value of n:%p \n", n);
+	printf("the value of address n %p \n, n");
+*n = 98;
+}
