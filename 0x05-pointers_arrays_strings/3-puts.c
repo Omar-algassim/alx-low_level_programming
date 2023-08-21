6a47c2058edba5c2d63b71a081ce9e2d66b55a7f
Omar-algassim <omarggg2@gmail.com>
2023-08-21 15:50:37 -0700

    update array

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - to print stdout
+ * written by omar
+ * @str: the string stdout
+ * Return: alyaws 0
+ */
+
+void _puts(char *str)
+{
+char str[7] = "stdout";
+
+printf("%s\n", str);
+
+return (0);
+}
