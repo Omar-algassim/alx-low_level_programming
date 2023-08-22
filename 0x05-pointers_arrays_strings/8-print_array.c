53f80cb43dd982b8eacf5476790c0490041295f9
Omar-algassim <omarggg2@gmail.com>
2023-08-21 19:15:43 -0700

    update array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - print elemnts number in array
+ * @a: name of array 
+ * @n: number of elemnts
+ * Return: aways 0
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i <= n; i++)
+{
+	printf("%d", a[n]);
+	if (a[n] != '\0')
+	printf(", ");
+}
+printf ("\n");
+}
