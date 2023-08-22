7ca5fdd554686f5e080ac37df9e5f59cfdd4b1e5
Omar-algassim <omarggg2@gmail.com>
2023-08-21 18:08:20 -0700

    update array

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - print the last half of string
+ * @str: the string array
+ * Return: 0 
+ */
+
+void puts_half(char *str)
+{
+int c, i, h, j;
+
+c = 0;
+while (str[c] != '\0')
+{
+c++;
+}
+j = c;
+h = (j - 1) / 2;
+
+for (i = h; i <= c; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
