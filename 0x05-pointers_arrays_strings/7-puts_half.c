a9e40e82d664b51555b10a78cb54c82d56112933
Omar-algassim <omarggg2@gmail.com>
2023-08-21 18:48:31 -0700

    update array

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,17 +16,20 @@ c++;
 }
 j = c;
 if (j % 2 == 0)
-
+{
 h = j / 2;
 for (i = h; str[i] != '\0'; i++)
 {
 _putchar(str[i]);
 }
+}
 else
+{
 h = (j - 1) / 2;
 for (i = h + 1; str[i] != '\0'; i++)
 {
 _putchar(str[i]);
+}
 }
 _putchar('\n');
 }
