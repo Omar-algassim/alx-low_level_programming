8588968ed599430eda7a60afeb865d7e70a9db1f
Omar-algassim <omarggg2@gmail.com>
2023-08-23 10:43:14 -0700

    c lang

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat : concantenates tow string
+ * @str: string number one 
+ * @dest: the string append in 
+ * return:the value of dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+
+i = 0;
+j = 0;
+
+while (src[i] != '\0')
+
+	i++;
+while (dest[j] != '\0')
+{
+src[i] = dest[j];
+j++;
+}
+dest[j] = '\0';
+return (dest);
