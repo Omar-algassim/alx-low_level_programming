ccf24514c410419b652742199f5ca9295add49e4
Omar-algassim <omarggg2@gmail.com>
2023-08-23 12:20:24 -0700

    c lang

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,20 +4,28 @@
  * _strcat - concantenates tow string
  * @src: string number one
  * @dest: the string append in
+ * @n: number of byte append to 
  * Return:the value of dest
  */
 
-char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src)
 {
 int i, j;
 
 i = 0;
+j = 0;
 
 while (dest[i] != '\0')
-{
-	i++;
-}
-dest[i] = src[n];
 
+        i++;
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+
+dest[i] = '\0';
 return (dest);
 }
+"0-strcat.c" 29L, 337C                                                                                              29,1          All
