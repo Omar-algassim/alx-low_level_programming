2e01ff1df4a686d9b783103c32dfc6637bc8c2c5
Omar-algassim <omarggg2@gmail.com>
2023-08-23 17:47:45 -0700

    c lang

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,30 +1,30 @@
 #include "main.h"
 
 /**
- * _strcat - concantenates tow string
- * @src: string number one
- * @dest: the string append in
- * @n: number of byte append to 
- * Return:the value of dest
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the resulting string
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
-int i, j;
+int i;
 
 i = 0;
-j = 0;
 
-while (dest[i] != '\0')
-
-        i++;
-while (src[j] != '\0' && j < n)
-{
-dest[i] = src[j];
-j++;
-i++;
+while (src[i] != '\0' && i < n)
+	{
+	dest[i] = src[i];
+	i++;
 }
-dest[i] = '\0';
+
+while (i < n)
+{
+	dest[i] = '\0';
+	i++;
+}
 
 return (dest);
 }
