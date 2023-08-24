e0f97b4b6f5d1a438461b659261b8297d384af22
Omar-algassim <omarggg2@gmail.com>
2023-08-24 11:43:24 -0700

    c lang

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,9 +2,8 @@
 
 /**
  * string_toupper - function chang the lowercase string to uppercas
- *
- *@s: the string will be chinging to uppercase
- *Return: the new string
+ * @s: the string will be chinging to uppercase
+ * Return: the new string
  */
 char *string_toupper(char *s)
 {
@@ -12,8 +11,8 @@ int i;
 
 for (i = 0; s[i] != '\0'; i++)
 {
-	if (i >= 'a' && i <= 'z')
 	s[i] = s[i] - 32;
 }
-return (s);
+return(s);
 }
+
