b064e43af8b2b7ea2dd1e46bacde8b83bf1d26b7
Omar-algassim <omarggg2@gmail.com>
2023-08-24 07:10:19 -0700

    c lang

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - revers the contents
+ * @a: the array
+ * @n: the numer of elemnt
+ * Return: always 0
+ */
+
+void reverse_array(int *a, int n)
+{
+while (n != 0)
+{
+	_putchar(a[n]);
+	n--;
+}
+return (0);
+
