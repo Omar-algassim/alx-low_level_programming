44711543fe87f8b783c9ed5cf49f98c944d52803
Omar-algassim <omarggg2@gmail.com>
2023-08-24 04:16:44 -0700

    c lang

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - function cpies that string
+ * @dest: var string copy in
+ * @src: string copied from
+ * @n: byts of src
+ * Return: dest value
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+   for (i = 0; i < n && src[i] != '\0'; i++)
+   {
+    	   dest[i] = src[i];
+   }
+    for ( ; i < n; i++)
+        dest[i] = '\0';
+
+   return dest;
+}
