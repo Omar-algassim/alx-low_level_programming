ef0ef926720b77d745f75015b9b61592f690ef59
Omar-algassim <omarggg2@gmail.com>
2023-08-24 04:05:02 -0700

    c lang

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,16 +15,17 @@ int i, j;
 i = 0;
 j = 0;
 while (dest[i] != '\0')
-	{
-	dest[i] = src[i];
+{
+	
 	i++;
 }
 
 while (src[j] != '\0' && j < n)
 {
-	dest[i] = '\0';
+	dest[i] = src[i]
 	i++;
+	j++;
 }
-
+dest[i] = '\0';
 return (dest);
 }
