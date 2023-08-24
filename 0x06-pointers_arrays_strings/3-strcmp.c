3766909a1289503e0d35cac8d5fd9af4c1a53967
Omar-algassim <omarggg2@gmail.com>
2023-08-24 05:09:23 -0700

    c lang

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare tow string
+ * @s1: the string number 1
+ * @s2: the string number 2
+ * Return: return 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int i, j, v1, v2;
+
+for (i = 0; s1[i] != '\0'; i++)
+{
+	i = vi;
+}
+for (j = 0; s2[j] != '\0'; j++)
+{
+	j = v2;
+}
+if (v1 > v2)
+	printf("15\n");
+if else(v1 < v2)
+	printf("-15\n");
+else
+	printf("0\n");
+
+return (0);
+}
