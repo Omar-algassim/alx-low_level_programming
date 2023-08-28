18841e56046fbf1c0407e271affc66fd9aeb207e
Omar-Algassim <omarggg2@gmail.com>
2023-08-28 18:35:52 +0000

    pointer in pointer

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,21 @@
-#include "main.h"
+nclude "main.h"
 
 /**
- * _memset - function fills the first n bytes of the memory area pointed to by s with the constant byte b
- * @s:pointed by constant
- * @n: number of byte
- * @b: constant b
- * Return: value to s
- */
-
+ *  * *_memset - fills memory with a constant byte
+ *   * @s: memory area to be filled
+ *    * @b: char to copy
+ *     * @n: number of times to copy b
+ *      *
+ *       * Return: pointer to the memory area s
+ *        */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+        unsigned int i;
 
-for (i = 0; i < n; i++)
-{
-    s[i] = b;
-}
- return (s);
+            for (i = 0; i < n; i++)
+                    {
+                                s[i] = b;
+                                    }
+
+                return (s);
 }
