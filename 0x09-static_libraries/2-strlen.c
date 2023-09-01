681d9db1084fbb2fd7380989f4c743c928ae7b01
Omar-algassim <omarggg2@gmail.com>
2023-09-01 13:48:08 -0700

    update

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of string
+ * written by omar
+ * @s: the vlaue of length
+ * Return: the length of string
+ */
+int _strlen(char *s)
+{
+int i;
+
+i = 0;
+while (s[i] != '\0')
+{
+	i++;
+}
+return (i);
+}
