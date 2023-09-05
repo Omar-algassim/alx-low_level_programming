f23261e297a9e22b921796e4327543f8a57f6baf
Omar-algassim <omarggg2@gmail.com>
2023-09-05 13:18:18 -0700

    argv upload

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print program name
+ * @argc: argument counter
+ * @argv: argument value
+ * Return: always zero
+ */
+
+int main (int argc, char *argv[])
+{
+printf ("the program name is:%c" , argv[0]);
+}
