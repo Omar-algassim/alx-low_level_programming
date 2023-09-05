bee97e928f9dc62fa9009cd01ca6df204f3a3538
Omar-algassim <omarggg2@gmail.com>
2023-09-05 11:59:36 -0700

    upload

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** free_grid -  function that frees a 2 dimensional grid
+/** 
+ * free_grid - function that frees a 2 dimensional grid
  * @grid: tow diamantion
  * @height: heighe of grid
  * Return: null
@@ -13,8 +14,9 @@ void free_grid(int **grid, int height)
 if (grid == 0 || height == 0)
 	return;
 
+
 for (i = 0; i < height; i++)
 	free(grid[i]);
 
-	free(grid);
+free(grid);
 }
