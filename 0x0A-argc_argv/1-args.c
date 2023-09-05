b44f94f10b8fe5db2b45c4be8afa150938d52d56
Omar-algassim <omarggg2@gmail.com>
2023-09-05 13:55:07 -0700

    argv upload

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print number of argument
+ * @argc:argument counter
+ * @argv: argument value
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[] __attribtude__((unused)))
+{
+	printf("%d \n", argc);
+	return(0);
+}
