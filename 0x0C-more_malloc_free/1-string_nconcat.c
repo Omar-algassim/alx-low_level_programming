11e2440deab00ee251ea0c066c61416aeb219f7e
Omar-algassim <omarggg2@gmail.com>
2023-09-07 07:58:43 -0700

    upload

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,31 +18,25 @@ i = 0;
 j = 0;
 	
 while (s1 && s1[len])	
-{
 	len++;
-}
 while (s2 && s2[len1])
-{
 	len1++;
-}
+
 if (len1 >= n)
 	memo = malloc(sizeof(char) * (len + len1 + 1));
 	else
 	memo = malloc(sizeof(char) * (len + n + 1));
-if (!memo)
+if (memo == NULL)
 	return (NULL);
 
-	while (i < len)
-{	
+	while (i < len)	
 	memo[i] = s1[i];
 	i++;
-}
 while (n < len1 && i < (len + n))
-{
 	memo[i] = s2[j];
 	i++;
 	j++;
-	}
+
 while (n >= len1 && i < (len1 +len))
 	memo[i] = s2[j];
 	i++;
