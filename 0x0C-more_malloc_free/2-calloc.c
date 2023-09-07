a4120ac500315d6cfd345597ef566f10541e12a6
Omar-algassim <omarggg2@gmail.com>
2023-09-07 14:50:25 -0700

    upload

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,3 +8,39 @@
  *
  * Return: pointer to the memory area s
  */
+
+void *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *m;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	m = malloc(size * nmemb);
+
+	if(m == 0)
+		return (NULL);
+	_memset(m, 0, nmemb * size);
+
+	return (m);
+}
+
+
