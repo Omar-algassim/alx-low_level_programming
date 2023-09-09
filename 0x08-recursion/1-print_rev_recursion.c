c9f4df0da5d59952a6b5e3a642811e586fb89c66
Omar-algassim <omarggg2@gmail.com>
2023-09-09 04:59:19 -0700

    update

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev_recursion - print string in reverse
+ * @s: string will print
+ * Return:always 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchr('\n');
+	return;
+	}
+_putchar(*s);
+_print_rev_recursion(s - 1);
+}
