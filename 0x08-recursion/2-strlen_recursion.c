530d93e9dfecf82f504f02d3a874645430964c8b
Omar-algassim <omarggg2@gmail.com>
2023-09-09 05:57:48 -0700

    update

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,14 +8,11 @@
 
 int _strlen_recursion(char *s)
 {
-	 int i;
+	 
 
 	if (*s != '\0')
 	{
-	
-
-	i = _strlen_recursion(s + 1);
-	return (i);
+	return (1 + _strlen_recursion(s + 1));
 	}
 	return (0);
 }
