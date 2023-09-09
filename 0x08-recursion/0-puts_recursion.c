d6eddaf7923aa6f40be022a3b51aaec8db8c9c55
Omar-algassim <omarggg2@gmail.com>
2023-09-09 04:52:17 -0700

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * puts_recursion - print string follwed by new line
- * @s: the string should
- * Return: always 0
+ *  _puts_recursion - print string follwed by new line
+ *  @s: string that should print
+ *  Rrturn: always 0
  */
-
 void _puts_recursion(char *s)
 {
 
 	if (*s == 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 		return;
 	}
-	_putchar(*s);
-
-	 _puts_recursion(s + 1);
+_putchar(*s);
+_puts_recursion(s + 1);
 }
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,9 @@
-#define MAIN_H
 #ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -10,5 +11,4 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int _putchar(char c)
 #endif
