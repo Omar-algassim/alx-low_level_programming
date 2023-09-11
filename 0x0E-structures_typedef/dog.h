035f6b5a4e73aa3e9baa3737645844210cd7c3f1
Omar-algassim <omarggg2@gmail.com>
2023-09-11 21:52:47 +0200

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - description for a pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 char *name;
