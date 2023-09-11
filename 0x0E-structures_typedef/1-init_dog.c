7b023e0c5201f8c7a08aa61caafa65ba1f81dbf4
Omar-algassim <omarggg2@gmail.com>
2023-09-11 22:37:39 +0200

    updat

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,12 +8,12 @@
  * @owner:owner of the dog
  * Return: always 0
 */
- void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d != 0)
-    {
+if (d != 0)
+{
 d->name = name;
 d->age = age;
 d->owner = owner;
-    }
+}
 }
