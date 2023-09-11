8b3f2ffd017eabdb0816468aa7113a3cf095ac71
Omar-algassim <omarggg2@gmail.com>
2023-09-11 19:32:55 +0200

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,11 @@
 
 /**
  * main - print the program name
- * 
+ *
  * return:always 0
 */
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
