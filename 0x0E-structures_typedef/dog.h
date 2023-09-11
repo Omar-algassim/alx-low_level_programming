095dc512c1ddc531c52c822d39bc6ec2e7b80033
Omar-algassim <omarggg2@gmail.com>
2023-09-11 23:26:53 +0200

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,13 @@
  *
  * Description: Longer description
  */
-struct dog
+typedef struct dog dog_t
 {
 char *name;
 float age;
 char *owner;
 };
-typedef struct dog dog_t
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
