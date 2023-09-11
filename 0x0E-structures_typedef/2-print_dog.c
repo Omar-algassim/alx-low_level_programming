383c965f69ae9a642495a2eada08d7d91012a53a
Omar-algassim <omarggg2@gmail.com>
2023-09-11 14:09:57 -0700

    uodate

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,38 +3,37 @@
 /**
  * print_dog - print the dog info
  * @d:pointer to struct dog
- * 
+ *
  * Return: always 0
 */
 void print_dog(struct dog *d)
 {
-if (d == 0)
+	if (d == 0)
 {
-    printf("NULL\n");
+	printf("NULL\n");
 }
 if (d->name == 0)
 {
-    printf("nil\n");
+	printf("nil\n");
 }
 else
 {
-    printf("name: %s\n", d->name);
+	printf("name: %s\n", d->name);
 }
 if (d->age == 0)
 {
-    printf("nil\n");
+	printf("nil\n");
 }
 else
 {
-    printf("age: %f\n", d->age);
+	printf("age: %f\n", d->age);
 }
 if (d->owner == 0)
 {
-    printf("nil\n");
+	printf("nil\n");
 }
 else
 {
-    printf("owner: %s\n", d->owner);
+	printf("owner: %s\n", d->owner);
 }
-
 }
