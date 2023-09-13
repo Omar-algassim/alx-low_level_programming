1081bf04e0b4720fb7b4a573cd6a3dc661d0685d
Omar-algassim <omarggg2@gmail.com>
2023-09-13 19:26:55 +0200

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,8 +8,8 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-    if (cmp && array && size < 0)
-    return (-1);
+
+if (array && cmp && size < 0)
 for (i = 0; i < size; i++)
 if (cmp(array[i]))
 return (i);
