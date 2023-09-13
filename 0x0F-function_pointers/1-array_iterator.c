46a1a607a1e00e2ffa3901211c48923d3a7a085a
Omar-algassim <omarggg2@gmail.com>
2023-09-13 17:58:29 +0200

    update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,18 +1,18 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
  * array_iterator - print the array
  * @size: the size of array
  * @array: the array will printed
+ * @action: the function want to run
  * Return: nothing
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
     int i;
-    
-    if (array != 0 && action != 0)
+    if (array != 0 && action != 0 && size >= 1)
     {
     for (i = 0; i < size; i++)
     action(array[i]);
-    }   
+    }
 }
