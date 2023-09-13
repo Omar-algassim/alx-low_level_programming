31ffa1ae505ab1ba4aca30beadb8c76249a41c4a
Omar-algassim <omarggg2@gmail.com>
2023-09-14 00:44:05 +0200

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,34 +1,44 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * calc - function that test the program
- * @operation: the operation you have to use
- * @num1: number one to use
- * @num2: number 2 to use
- * Return: the value of operation 
-*/
-int main (int argc, char **argv)
+ * main - performs simple operations
+ *
+ * @argv: pointer to the argument that enter from the user as char type
+ * @argc: tell us the amount of arguments int type
+ *
+ * Return: an integer with the result of a operation
+ */
+int main(int argc, char *argv[])
 {
- int (*operator)(int, int), a, b;
-
-if (argc != 4)
-{
-printf("Error \n");
-exit (98);
-}
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-
-operator = get_op_func(argv[2]);
-
-if (!operator)
-printf("Eroor \n"), exit (99);
-
-if (b == 0 && (*argv[2] == '/' || *argv[2] == '%'))
-printf("Eroor\n"), exit (100);
-
-printf("%d\n", operator(a, b));
-return (0);
+	int (*funoperator)(int a, int b);
+	/* when the user enter the arguments there´s must be four elements */
+	/* the name of the file + first number + operator + second number */
+	/* follow first condition of the task */
+	/* if the number or arguments is wrong, print error */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	/* funoperator is gonna be equal to the pointer function */
+	/* in the argument number 2 that is the position of the operator */
+	/*if is not one of the operators mentioned else print Error */
+	funoperator = get_op_func(argv[2]);
+	if (!funoperator || strlen(argv[2]) != 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	/* add last condition, if user try to divides using % or / is Error */
+	/* that means, if *argv in position 2 that are operators */
+	/* receives % or / operators program prints Error */
+	/* AND if user is trying to divided by 0 is Error too */
+	/* e.g 2  % 0 , if argv in position 3is equal to cero is Error */
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	/* if any of the conditions theeennn : */
+	printf("%d\n", funoperator(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
