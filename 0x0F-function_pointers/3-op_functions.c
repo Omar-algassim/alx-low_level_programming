b71ec36e7e9411157d72ef1744ab4a0e07a620a9
Omar-algassim <omarggg2@gmail.com>
2023-09-13 21:02:31 +0200

    update

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,8 +1,54 @@
-#ifndef _OP_FUNCTIONS_C
-#define _OP_FUNCTIONS_C
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-#endif
+#include "3-calc.h"
+/**
+ * op_add - add tow number
+ * @a:integer number 1
+ * @b:integer number2
+ * Return: always 0
+*/
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * op_sub - subtruct tow number
+ * @a:integer number 1
+ * @b:integer number2
+ * Return: always a - b
+*/
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+/**
+ * op_mul - multiply tow number
+ * @a:integer number 1
+ * @b:integer number2
+ * Return: always a * b
+*/
+int op_mul(int a, int b)
+{
+    return (a * b)
+}
+
+/**
+ * op_div - divided tow number
+ * @a:integer number 1
+ * @b:integer number2
+ * Return: always a / b
+*/
+int op_div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * op_mod - modulo tow number
+ * @a:integer number 1
+ * @b:integer number2
+ * Return: always a % b
+*/
+int op_mod(int a, int b)
+{
+    return (a % b);
+}
