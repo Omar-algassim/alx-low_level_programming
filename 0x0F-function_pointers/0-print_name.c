48a04ac390be70ea2e3a8a01951a3c938ca3bb99
Omar-algassim <omarggg2@gmail.com>
2023-09-13 17:05:12 +0200

    function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
 #include "function_pointers.h"
 
 /**
-* print_name - function print the name
-* @name: name should to print
-* @f:pointer to functuin
-* Return: the name
-*/
+ * print_name - writes the character c to stdout
+ * @name: String
+ * @f: Pointer to function with char parameter and no return
+ *
+ * Return: Nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
-if (f != 0)
-{
-f(name);
-}
+	if (f != 0)
+		f(name);
 }
