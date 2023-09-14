96c4210a0ded67132eb4c1800037369bf15f56cb
Omar-algassim <omarggg2@gmail.com>
2023-09-14 05:54:43 -0700

    sum function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,21 +3,21 @@
 /**
  * sum_them_all - sum the numbers in var
  * @n:the number of integer
- * Return: the value of sum 
+ * Return: the value of sum
 */
 int sum_them_all(const unsigned int n, ...)
-{ 
-    va_list num;
-    unsigned int i, sum;
+{
+	va_list num;
+	unsigned int i, sum;
 
-if(n == 0)
-return(0);
+if (n == 0)
+return (0);
 
 va_start(num, n);
-sum = 0; 
-  for ( i = 0; i < n; i++)
-        sum += va_arg (num, int);
-        
-va_end (num);
+sum = 0;
+for (i = 0; i < n; i++)
+	sum += va_arg(num, int);
+
+va_end(num);
 return (sum);
 }
