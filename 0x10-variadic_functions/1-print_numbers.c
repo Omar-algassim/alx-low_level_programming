cab37686b2cacaed30e3664c880bda38b9e69376
Omar-algassim <omarggg2@gmail.com>
2023-09-14 15:08:26 +0200

    print_numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,19 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - print integer in sepatator
+ * @separator: the string will be print
+ * @n:number of integer
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+    int i;
+va_list num;
+if (separator != NULL)
+va_start(num, n)
+for (i = 0; i < n; i++)
+{
+num = va_arg(separator, char);
+printf("%d \n", num);
+}
+printf("\n");
+}
