43f94cbc5d9251b3b702027032928eec3d6109bc
Omar-algassim <omarggg2@gmail.com>
2023-09-14 15:40:34 +0200

    print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,11 +11,11 @@ va_list num;
 
 if (separator != NULL)
 va_start(num, n);
-for (i = 0; i >= n; i++)
+for (i = 0; i <= n; i++)
 {
-    i = va_arg(num, int);
+i = va_arg(num, int);
 printf("%d, ", i);
-va_end(num);
 }
+va_end(num)
 printf("\n");
 }
