fbaae3aece71588e09a9e82c553f612e961027cc
Omar-algassim <omarggg2@gmail.com>
2023-09-15 05:19:03 -0700

    dog info

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,15 @@
  *
  * Description: Longer description
  */
-typedef struct dog dog_t
+ struct dog
 {
 char *name;
 float age;
 char *owner;
 };
-
-
+typedef dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
