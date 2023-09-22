0c38c5a33bccbc0adebf70b776b612c433152fb5
Omar-algassim <omarggg2@gmail.com>
2023-09-21 18:10:06 -0700

    third code

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_node - add new node in the head
+ * @head:the first node
+ * @str: the string
+ * Return:the address of the new elemnt
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *cpy;
+	int len;
+	list_t *new;
+
+	len = 0;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+
+		return (NULL);
+
+	cpy = strdup(str);
+	if (cpy == NULL)
+	{
+		free (new);
+		return (NULL);
+	}
+	while (str[len])
+	{
+		len++;
+	new->str = cpy;
+	new->len = len;
+	new->next = *head;
+	}
+	*head = new;
+	
+return (new);
+}
