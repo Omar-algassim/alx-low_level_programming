c7be1f7e5472457c09f76f8c961b695734183671
Omar-algassim <omarggg2@gmail.com>
2023-09-26 04:26:56 -0700

    six code

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function delet the head node
+ * @head: pointer to the head
+ * Return:the value of head node or 0if empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *temp;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		temp = *head;
+		*head = temp->next;
+		n = temp->n;
+		free(temp);
+		return (n);
+	}
+}
