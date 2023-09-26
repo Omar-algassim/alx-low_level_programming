bda06fa073b0236e84e399295d704bea15f845da
Omar-algassim <omarggg2@gmail.com>
2023-09-26 04:51:41 -0700

    seven code

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_nodeint - return specific node
+ * @head: the head of linked list
+ * @index: the number of wantted node
+ * Return: the n node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp;
+
+	temp = head;
+	if (!index)
+	{
+		return (NULL);
+	}
+	else
+	while (i != index)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return (temp);
+}
+
