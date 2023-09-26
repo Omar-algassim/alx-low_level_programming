9560760894679cf4b561cfc341b7ad5b9443c860
Omar-algassim <omarggg2@gmail.com>
2023-09-26 05:01:32 -0700

    update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 
 	temp = head;
-	if (!index && head == NULL)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
@@ -22,6 +22,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		temp = temp->next;
 		i++;
+		if (head == NULL)
+			return (NULL);
 	}
 	return (temp);
 }
