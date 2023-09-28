75eb63a681e496a181d0eea747176cdc26e8a284
Omar-algassim <omarggg2@gmail.com>
2023-09-28 15:02:39 -0700

    update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,13 +4,13 @@
   * set_bit - chang bit to 1
   *@n: the number
   *@index:the number of bit
-  *Returns: 1 if it worked, or -1 if an error occurred
+  *Return: 1 if it worked, or -1 if an error occurred
   */
 
-  int set_bit(unsigned long int *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int h;
-	
+
 	h = 1;
 	if (!n || index > 63)
 		return (-1);
