980bf4d4058150843a4ebe022c3c228e7a0ab51a
Omar-algassim <omarggg2@gmail.com>
2023-09-28 15:56:27 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,7 +20,7 @@ unsigned int binary_to_uint(const char *b)
 
 	while (i)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i - 1] != '0' && b[i - 1] != '1' && b[i - 1] == NULL)
 		{
 			return (0);
 		}
