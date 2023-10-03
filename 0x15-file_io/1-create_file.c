9c7c06da7881d496efcd8ded0236b25efc750830
Omar-algassim <omarggg2@gmail.com>
2023-10-02 17:06:03 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,24 +7,28 @@
   *Return:eturns: 1 on success, -1 on failure
   if filename is NULL return -1
   */
-
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	int i = 0;
+	int i = 0, file;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	if (!text_content)
-		text_content = "";
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
 
-	while (text_content)
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[i] != '\0')
 	{
 		i++;
 	}
 
-	write(file, text_content, i);
-return (1);
-}
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
+	return (1);
+}
