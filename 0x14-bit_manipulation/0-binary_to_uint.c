990d662aa55e03126a78ae89901bec034a0d8523
Omar-algassim <omarggg2@gmail.com>
2023-10-26 14:37:15 -0700

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,14 +10,16 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i, oct, hex;
 
-	i = strlen(b);
+	i  = 0;
 	oct = 0;
 	hex = 1;
 
 	if (b == NULL)
 		return (0);
-	if (i == 0)
-		return (0);
+	while (b[i])
+	{
+		i++;
+	}
 	while (i)
 	{
 		if (b[i - 1] != '0' && b[i - 1] != '1')
