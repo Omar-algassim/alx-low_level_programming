522013a010b9975951eab4176ea33ba3a39a20fe
Omar-algassim <omarggg2@gmail.com>
2023-10-30 08:04:33 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,12 +13,17 @@
 int create_file(const char *filename, char *text_content)
 {
 	int op, w;
-	int len = strlen(text_content);
+	int len = 0;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
+
+	while (text_content[len] != '\0')
+	{
+		len++;
+	}
 
 	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (op == -1)
