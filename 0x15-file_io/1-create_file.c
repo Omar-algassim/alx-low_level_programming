5fb4ec6169d371ec3c6b804a42ada5e3a2a4bfca
Omar-algassim <omarggg2@gmail.com>
2023-10-30 08:05:36 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - function create the file
  * @filename: the name of file will created
- * @text_content: is a NULL terminated string to write to the file 
+ * @text_content: is a NULL terminated string to write to the file
  * Return: Returns: 1 on success, -1 on failure
  * (file can not be created, file can not be written, write fails, etc)
  * if filename is NULL return -1
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (op == -1)
 		return (-1);
-	
+
 	w = write(op, text_content, len);
 	if (w == -1)
 		return (-1);
