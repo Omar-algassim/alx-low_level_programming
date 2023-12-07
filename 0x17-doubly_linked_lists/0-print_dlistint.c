92c9ed6e9c70b95013b2e747b53b6f95a8059ffa
Omar-algassim <omarggg2@gmail.com>
2023-12-07 16:34:57 +0200

    betty update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,14 +8,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
-    int i = 0;
+const dlistint_t *current = h;
+int i = 0;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        i++;
-        current = current->next;
-    }
-    return (i);
+while (current != NULL)
+{
+printf("%d\n", current->n);
+i++;
+current = current->next;
+}
+return (i);
 }
