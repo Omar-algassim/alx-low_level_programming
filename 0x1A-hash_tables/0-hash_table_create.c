572b6d2405b3eaa6aeeed01f4d50fe4bf01ed3e7
Omar-algassim <omarggg2@gmail.com>
2023-12-22 14:36:58 +0200

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,18 +9,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
-	long unsigned int i = 0;
+	unsigned int i = 0;
 
 	if (table != NULL)
 	{
 		table->size = size;
-		table->array = malloc(size * sizeof(hash_node_t*));
+		table->array = malloc(size * sizeof(hash_node_t *));
 		if (table->array == NULL)
 			return (NULL);
 	}
 	else
 		return (NULL);
-	for (; i < table->size; i++) 
+	for (; i < table->size; i++)
 	{
 	table->array[i] = NULL;
 	}
