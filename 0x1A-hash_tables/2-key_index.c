0a602bdad18ee375c7353b24b5e26cdad77194ff
Omar-algassim <omarggg2@gmail.com>
2023-12-22 15:37:18 +0200

    update

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - give the index key
+ * @key: the key of index
+ * @size: the size of table
+ * Return: the index of key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+{
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+
+	return (index);
+}
