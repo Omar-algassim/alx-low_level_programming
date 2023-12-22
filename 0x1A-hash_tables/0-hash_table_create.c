12d8581c2f21e27cb956d6255876debd97f4602b
Omar-algassim <omarggg2@gmail.com>
2023-12-22 02:08:20 +0200

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,13 +7,13 @@
  * Return: the new hash_tables
  */
 
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t new = malloc(sizeof(hash_table_t));
+	hash_table_t *new = malloc(sizeof(hash_table_t));
 	if (new != NULL)
 	{
 		new->size = size;
-		new->array = calloc(new->size, sizeof(hash_node_s));
+		new->array = calloc(new->size, sizeof(hash_node_t));
 	}
 
 	return (new);
