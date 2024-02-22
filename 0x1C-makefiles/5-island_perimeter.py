d71b2d42d0f83dd551dfd24e81518fa3b6a35f8f
Omar-algassim <omarggg2@gmail.com>
2024-02-22 05:11:24 +0200

    update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+""" define the island perimeter"""
+
+
+def island_perimeter(grid):
+    """ calculate the perimeter of island in grid"""
+
+    perimeter = 0
+    horizontal = -1
+    vertical = -1
+
+    for row in grid:
+        horizontal += 1
+        for item in row:
+            vertical += 1
+            if item == 1:
+                cell = 1
+                perimeter = 3
+                while cell != 0:
+                    if grid[horizontal - 1][vertical] == 1:
+                        perimeter += 2
+                        cell = grid[horizontal - 1][vertical]
+                        horizontal -= 1
+                    elif grid[horizontal + 1][vertical] == 1:
+                        perimeter += 2
+                        cell = grid[horizontal + 1][vertical]
+                        horizontal += 1
+                    elif grid[horizontal][vertical - 1] == 1:
+                        perimeter += 2
+                        cell = grid[horizontal][vertical - 1]
+                        vertical -= 1
+                    elif grid[horizontal][vertical + 1]:
+                        perimeter += 2
+                        cell = grid[horizontal][vertical + 1]
+                        vertical += 1
+                    elif:
+                        cell = 0
+    return perimeter
